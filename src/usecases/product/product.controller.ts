955a76348385edf0258df20ac7b88b23181664da
RenatoLomba <63821245+RenatoLomba@users.noreply.github.com>
2021-08-13 21:26:45 -0300

    removed authorization to list products

--- src/usecases/product/product.controller.ts
+++ src/usecases/product/product.controller.ts
@@ -1,6 +1,5 @@
 import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
 import { AuthAdminGuard } from 'src/common/guards/auth-admin.guard';
-import { AuthUserGuard } from 'src/common/guards/auth-user.guard';
 import { ProductService } from 'src/usecases/product/product.service';
 import { CreateProductDto } from './dto/create-product.dto';
 
@@ -9,13 +8,11 @@ export class ProductController {
   constructor(private readonly productService: ProductService) {}
 
   @Get()
-  @UseGuards(AuthUserGuard)
   async products() {
     return this.productService.findMany({});
   }
 
   @Get(':id')
-  @UseGuards(AuthUserGuard)
   async product(@Param('id') _id: string) {
     return this.productService.findUnique({ _id });
   }
